aa5ad27006df90e5589165d5cc7e88eb6166c985
Bimandi <it24100203@my.sliit.lk>
2025-05-07 11:37:48 +0530

    successfully-loggedin text added

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -3,12 +3,12 @@ function validateForm() {
     const password = document.getElementById("password").value.trim();
 
     const users = [
-        { name: "Admin", email: "bimandi@gmail.com", password: "bimandi123", role: "admin" },
-        { name: "User1", email: "thanuja@gmail.com", password: "thanuja123", role: "admin" },
-        { name: "User2", email: "kushani@gmail.com", password: "kushani123", role: "admin" },
-        { name: "User3", email: "tharushi@gmail.com", password: "tharushi123", role: "admin" },
-        { name: "User4", email: "keshali@gmail.com", password: "keshali123", role: "admin" },
-        { name: "User5", email: "abey@gmail.com", password: "abey123", role: "admin" }
+        { name: "Admin", email: "bima@gmail.com", password: "bima123", role: "admin" },
+        { name: "User1", email: "thanu@gmail.com", password: "thanu123", role: "admin" },
+        { name: "User2", email: "kush@gmail.com", password: "kush123", role: "admin" },
+        { name: "User3", email: "tharu@gmail.com", password: "tharu123", role: "admin" },
+        { name: "User4", email: "kesh@gmail.com", password: "kesh123", role: "admin" },
+        { name: "User5", email: "abeyy@gmail.com", password: "abeyy123", role: "admin" }
     ];
 
     const user = users.find(u => u.email === email && u.password === password);
@@ -36,7 +36,7 @@ function handleLogin() {
     if (email === "admin@example.com" && password === "1234") {
         document.getElementById('login-message').style.display = "block";
 
-        // Optional: Redirect after a short delay
+        //optional
         setTimeout(() => {
             window.location.href = "homepage.html";
         }, 1500);
