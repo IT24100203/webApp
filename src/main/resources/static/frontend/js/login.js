e630d5a6ca84e77ea4b223b2466259e706e57320
Abeysekara T.T <it24100172@my.sliit.lk>
2025-05-21 22:30:03 +0530

    change url

--- src/main/resources/static/frontend/js/login.js
+++ src/main/resources/static/frontend/js/login.js
@@ -30,9 +30,9 @@ document.getElementById('login-form').addEventListener('submit', function(event)
                 sessionStorage.setItem('currentUser', JSON.stringify(data));
 
                 if (data.role === "admin") {
-                    window.location.href = '/frontend/html/landing-page-admin.html';
+                    window.location.href = '/webApp/static/frontend/html/landing-page-admin.html';
                 } else {
-                    window.location.href = '/frontend/html/landing-page-user.html';
+                    window.location.href = '/webApp/static/frontend/html/landing-page-user.html';
                 }
             } else {
                 throw new Error("Invalid user data received from API.");
